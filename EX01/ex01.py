e183e7d216479fb2e5ab76b52ff35d06fad15422
Yoon Kim <ky099714@gmail.com>
2016-03-21 11:07:52 +0900

    added ex01.py

--- EX01/ex01.py
+++ EX01/ex01.py
@@ -3,3 +3,8 @@ print "Hello World"
 print "Hello again"
 print "I like typing this"
 print "This is fun"
+print "Yay! Printing"
+print "I'd much rather than you 'not'"
+print 'I "said" do not touch this'
+
+print "Python"
