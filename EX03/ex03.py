3a2501880a113e7f55283f04dde04f1e262ae210
YoonKim <ky099714@gmail.com>
2016-03-22 12:35:57 +0900

    added comment ex03.py

--- EX03/ex03.py
+++ EX03/ex03.py
@@ -1,23 +1,56 @@
+# -*- coding:cp949
+# 이제 치킨의 수를 세겠음
+
 print "I will now count my chickens:"
 
+# 암탉이 27+24/6마리가 있다고함
+# 즉, 암탉은 31마리가 있었음
+
 print "Hens", 27+24/6
+
+# 수탉이 99-42*2%9(9로 나눈 나머지) 가 있었다고함
+# 계산해보니 수탉은 96마리
+
 print "Roosters", 99-42*2%9
+
+# 이제 계란의 수를 세보겠음
 
 print "Now I will count my eggs:"
 
+# 계란의 수가 5+4+1-3+7%3(3으로 나눈 나머지)-1/4+3개가 있었다고함
+# 계산해보니 11개
 print 5+4+1-3+7%3-1/4+3
+
+# 부등식 3+2<5-9가 참인가를 물어봄
 
 print "Is it true that 3+2<5-9?"
 
+# 거짓
 print 3+2<5-9
 
+# 3+2=5
 print "What is 3+2?", 3+2
+# 5-9 =-4
 print "What is 5-9?", 5-9
 
 print "Oh, that's why it's False."
 
 print "How about some more."
 
+# 부등식 5>-2 가 맞냐고 물어봄
+# 맞음
 print "Is it grater?", 5>-2
+# 부등식 5>=-2 가 맞냐고 물어봄
+# 맞음
 print "Is it greater or equal?", 5>=-2
+# 부등식 5<-2가 맞냐고 물어봄
+# 틀림
 print "Is it less or equal?", 5<=-2
+
+# 소수점 계산 1.4-2.7*3.5+6/2+3.5
+# 계산하면 -1.55
+print 1.4-2.7*3.5+6/2+3.5
+
+# 유효숫자가 2개인 10미만의 자연수 계산 3.0-2.0+9.0%4.0
+# 답은 2.0
+print 3.0-2.0+9.0%4.0
