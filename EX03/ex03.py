d633b778e0a210f30262128bd15dfb9695197928
YoonKim <ky099714@gmail.com>
2016-03-22 12:03:06 +0900

    added ex03.py

---
+++ EX03/ex03.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens:"
+
+print "Hens", 27+24/6
+print "Roosters", 99-42*2%9
+
+print "Now I will count my eggs:"
+
+print 5+4+1-3+7%3-1/4+3
+
+print "Is it true that 3+2<5-9?"
+
+print 3+2<5-9
+
+print "What is 3+2?", 3+2
+print "What is 5-9?", 5-9
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it grater?", 5>-2
+print "Is it greater or equal?", 5>=-2
+print "Is it less or equal?", 5<=-2
