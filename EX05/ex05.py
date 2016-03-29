b81638a0c41c998574536734f21b6e85ce46d1b3
YoonKim <ky099714@gmail.com>
2016-03-29 10:48:15 +0900

    added ex05_2

--- EX05/ex05.py
+++ EX05/ex05.py
@@ -13,5 +13,10 @@ print "He's %g centimiters tall. " % my_height_cm
 print "He's %g inches tall."  % my_height_inch
 print "He's %d kilograms heavy." % my_weight_kg
 print "Actually that's not too heavy"
-print "He's got %s eyes %s hair." % (my_eyes, my_hair)
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height_cm, my_weight_kg, my_age + my_height_cm + my_weight_kg)
+
+
