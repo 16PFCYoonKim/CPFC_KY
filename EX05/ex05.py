2d334628650fc2b2908d31fb3d36d818e29870fa
YoonKim <ky099714@gmail.com>
2016-03-29 10:30:47 +0900

    added ex3,4,5

---
+++ EX05/ex05.py
@@ -0,0 +1,15 @@
+my_name = 'Yoon Kim'
+my_age = 21 # not a lie
+my_height_cm = 180
+cm_to_inch = 1.0 / 2.54
+my_height_inch = my_height_cm * cm_to_inch
+my_weight_kg = 77
+my_eyes = "Black"
+my_teeth = "White"
+my_hair = "Black"
+# place holder
+print "Let's talk about %s." % my_name
+print "He's %g centimiters tall. " % my_height_cm
+print "He's %g inches tall."  % my_height_inch
+print "He's %d kilograms heavy." % my_weight_kg
+print "Actually that's not too heavy"]
