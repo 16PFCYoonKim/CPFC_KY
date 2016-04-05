2ffbbcd497369c666deb52f99f3a862e5ddc3f5e
YoonKim <ky099714@gmail.com>
2016-04-05 10:40:16 +0900

    ex07.py studydrill and added error note

--- EX07/ex07.py
+++ EX07/ex07.py
@@ -1,7 +1,8 @@
-print "Mary had a little lamb"
-print "Its fleece was white as %s." % 'snow'
-print "And everywhere that Mary went"
-print "." *10 # What'd that do?
+#-*-coding:cp949
+print "Mary had a little lamb"   #메리에게 작은 양이 있다고함
+print "Its fleece was white as %s." % 'snow'   #눈처럼 하얀 털을 가졌다고 함
+print "And everywhere that Mary went"   #메리가 갔던 모든 곳에
+print "." *10 # What'd that do? #..........
 
 end1 = "C"
 end2 = "h"
@@ -17,5 +18,5 @@ end11 = "e"
 end12 = "r"
 
 # Watch that comma at the end. try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6,
+print end1 + end2 + end3 + end4 + end5 + end6,   #이욚의 쉽표는 이어주는 역할을 함
 print end7 + end8 + end9 + end10 + end11 +end12
